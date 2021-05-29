9650f3ef66625abc736a70ffa1cfc50fd0834fae
Jared Mock <jared.mock@bthtech.com>
2021-05-29 01:37:18 -0500

    Integrate libcheat with draggable_div.
    Signed-off-by: Jared Mock <jared.mock@bthtech.com>

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-const uidivid = 'cheat_ui_div';
+export const draggable_div = {
+	move : function(divid,xpos,ypos){
+		divid.style.left = xpos + "px";
+		divid.style.top = ypos + "px";
+	},
+	startMoving : function(divid,/*container,*/evt){
+		evt = evt || window.event;
+		var posX = evt.clientX,
+			posY = evt.clientY,
+		divTop = divid.style.top,
+		divLeft = divid.style.left,
+		eWi = parseInt(divid.offsetWidth),
+		eHe = parseInt(divid.offsetHeight),
+		// cWi = parseInt(document.getElementById(container).style.width),
+		// cHe = parseInt(document.getElementById(container).style.height);
+		// document.getElementById(container).style.cursor='move';
+		// cWi = parseInt(document.body.style.width),
+		// cHe = parseInt(document.body.style.height);
+		cWi = parseInt(visualViewport.width),
+		cHe = parseInt(visualViewport.height);
+		divTop = divTop.replace("px","");
+		divLeft = divLeft.replace("px","");
+		var diffX = posX - divLeft,
+			diffY = posY - divTop;
+		document.onmousemove = function(evt){
+			evt = evt || window.event;
+			var posX = evt.clientX,
+				posY = evt.clientY,
+				aX = posX - diffX,
+				aY = posY - diffY;
+				if (aX < 0) aX = 0;
+				if (aY < 0) aY = 0;
+				if (aX + eWi > cWi) aX = cWi - eWi;
+				if (aY + eHe > cHe) aY = cHe -eHe;
+			draggable_div.move(divid,aX,aY);
+		}
+	},
+	stopMoving : function(container){
+		var a = document.createElement("script");
+		// document.getElementById(container).style.cursor='default';
+		document.onmousemove = function(){}
+	},
+}();
+
+const uidivid = "cheat_ui_div";
 (function() {
 	if(document.getElementById(uidivid)) return;
-	const div = document.createElement('div');
+	const div = document.createElement("div");
 	div.id = uidivid;
-	div.style="border-width:3px;background-color:#cae8ca;border-style:solid;position:fixed;padding:7px;bottom:0;right:0;max-width:450px;z-index:999"
+	div.style = "border-width:3px;background-color:#cae8ca;border-style:solid;position:absolute;padding:7px;top:0;left:0;max-width:450px;z-index:999"
+	div.onmousedown = function(event) { draggable_div.startMoving(this,event); };
+	div.onmouseup = function(event) { draggable_div.stopMoving(); };
 	document.body.appendChild(div);
 })();
 
@@ -15,13 +61,13 @@ export const libcheat = {
 	addTask: (task, toggleable=false, label="") => {
 		if(toggleable) {
 			const div = document.getElementById(uidivid);
-			const box = document.createElement('input');
-			const text = document.createElement('span');
+			const box = document.createElement("input");
+			const text = document.createElement("span");
 			text.innerText = label;
 			box.setAttribute("type", "checkbox");
 			div.appendChild(box);
 			div.appendChild(text);
-			div.appendChild(document.createElement('br'));
+			div.appendChild(document.createElement("br"));
 			const oldtask = task;
 			task = () => {if(box.checked) oldtask()}
 		}
@@ -30,10 +76,10 @@ export const libcheat = {
 	clearTasks: () => tasks = [],
 	addButton: (onClick, label) => {
 		const div = document.getElementById(uidivid);
-		const button = document.createElement('button');
+		const button = document.createElement("button");
 		button.innerText = label;
-		button.addEventListener('click', onClick);
+		button.addEventListener("click", onClick);
 		div.appendChild(button);
-		div.appendChild(document.createElement('br'));
+		div.appendChild(document.createElement("br"));
 	},
 }
